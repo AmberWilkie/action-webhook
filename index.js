9fc27adf2c736418212e65925afd57527c804074
amberwilkie <amber@amberwilkie.com>
2020-08-19 16:56:03 -0400

    Comment out console log, clean up

--- index.js
+++ index.js
@@ -4,19 +4,17 @@ const axios = require('axios').default
 
 const callWebhook = async _ => {
   try {
-    // `who-to-greet` input defined in action metadata file
     const url = core.getInput('webhook_url');
     console.log(`url: ${url}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
+
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    // console.log(`The event payload: ${payload}`);
     const response = await axios.post(url, payload)
-    console.log('AMBER ---------------------> ', response)
+    console.log('AMBER ---------------------> response: ', response)
     return response.json()
   } catch (error) {
-    console.log('AMBER ---------------------> ', error)
+    console.log('AMBER ---------------------> error:', error)
     core.setFailed(error.message);
   }
 }
